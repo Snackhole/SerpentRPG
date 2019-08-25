86a391589f4e73591335508f77f2b09a4cd057a6
Snackhole <>
2019-08-25 18:57:28 -0500

    Work on Wilderness Travel Manager interface

--- Source/Interface/WildernessTravelManagerWindow.py
+++ Source/Interface/WildernessTravelManagerWindow.py
@@ -1,3 +1,7 @@
+from PyQt5 import QtCore
+from PyQt5.QtGui import QIntValidator
+from PyQt5.QtWidgets import QLineEdit, QGridLayout, QLabel
+
 from Components.WildernessTravelManager import WildernessTravelManager
 from Interface.Window import Window
 
@@ -8,3 +12,36 @@ class WildernessTravelManagerWindow(Window):
 
         # Create Wilderness Travel Manager
         self.WildernessTravelManager = WildernessTravelManager()
+
+    def CreateInterface(self):
+        # Validator
+        self.Validator = QIntValidator()
+        self.Validator.setBottom(0)
+
+        # Current Supply Points Label
+        self.CurrentSupplyPointsLabel = QLabel("Supply Points")
+        self.CurrentSupplyPointsLabel.setMaximumWidth(70)
+
+        # Current Supply Points Line Edit
+        self.CurrentSupplyPointsLineEdit = QLineEdit()
+        self.CurrentSupplyPointsLineEdit.setValidator(self.Validator)
+        self.CurrentSupplyPointsLineEdit.setMaximumWidth(40)
+        self.CurrentSupplyPointsLineEdit.setAlignment(QtCore.Qt.AlignCenter)
+
+        # Supply Pool Label
+        self.SupplyPoolLabel = QLabel("Supply Pool")
+        self.SupplyPoolLabel.setMaximumWidth(70)
+
+        # Supply Pool Line Edit
+        self.SupplyPoolLineEdit = QLineEdit()
+        self.SupplyPoolLineEdit.setValidator(self.Validator)
+        self.SupplyPoolLineEdit.setMaximumWidth(40)
+        self.SupplyPoolLineEdit.setAlignment(QtCore.Qt.AlignCenter)
+
+        # Create Layout
+        self.Layout = QGridLayout()
+        self.Layout.addWidget(self.CurrentSupplyPointsLabel, 0, 0)
+        self.Layout.addWidget(self.CurrentSupplyPointsLineEdit, 0, 1)
+        self.Layout.addWidget(self.SupplyPoolLabel, 1, 0)
+        self.Layout.addWidget(self.SupplyPoolLineEdit, 1, 1)
+        self.Frame.setLayout(self.Layout)
