cd6e074996d4a27eb2523ff476a01e32c9930668
Snackhole <>
2019-08-25 17:26:24 -0500

    Progress on DieClock

--- Source/Components/DieClock.py
+++ Source/Components/DieClock.py
@@ -2,6 +2,22 @@ from Components.DiceRoller import DiceRoller
 
 
 class DieClock:
-    def __init__(self):
+    def __init__(self, ComplicationThreshold=10, MaximumValue=21):
+        # Store Parameters
+        self.ComplicationThreshold = ComplicationThreshold
+        self.MaximumValue = MaximumValue
+
+        # Initial Value
+        self.Value = 0
+
         # Dice Roller
         self.DiceRoller = DiceRoller()
+
+    def IncreaseClock(self, ValueIncrease=1):
+        self.Value += ValueIncrease
+        if self.Value > self.ComplicationThreshold:
+            ClockRoll = self.DiceRoller.RollDice(DieType=self.MaximumValue - 1)["Total"]
+            if ClockRoll < self.Value:
+                self.Value = 0
+                return True
+        return False
