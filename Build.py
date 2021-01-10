2444d05fe021720f8b2f9e9d24bda87c30ec4e32
Snackhole <37544796+Snackhole@users.noreply.github.com>
2021-01-10 17:02:22 -0600

    Bump version to 9

--- Build.py
+++ Build.py
@@ -7,7 +7,7 @@ import zipapp
 
 # Build Variables
 BuildVariables = {}
-BuildVariables["Version"] = "8"
+BuildVariables["Version"] = "9"
 BuildVariables["AppName"] = "SerpentRPG"
 BuildVariables["VersionedAppName"] = BuildVariables["AppName"] + " " + BuildVariables["Version"]
 
